1f83608b06f7883f47c10d9658ba4f4d4cee5549
fmanvegas <fmanvegas@gmail.com>
2020-08-20 16:51:21 -0700

    Add project files.

---
+++ MainViewModel.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TaskExtension
+{
+    public class MainViewModel : PropChange
+    {
+        public MainViewModel()
+        {
+            
+        }
+
+   
+        internal void StartTask()
+        {
+            int i = 2;
+
+            DoMyTask(i).AwaitVoid(whenVoidCompleted, whenVoidError);
+        }
+        private void whenVoidCompleted()
+        {
+            MessageBox.Show("Void Task Completed");
+        }
+        private void whenVoidError(Exception obj)
+        {
+            MessageBox.Show($"Void Task Failed {obj.Message}");
+        }
+
+
+        private string _output = "Nothing";
+        public string Output
+        {
+            get { return _output; }
+            set { _output = value; OnPropChange(); }
+        }
+
+
+        private async Task DoMyTask(int i)
+        {
+            await Task.Run(() =>
+            {
+                for (int x = 0;x < 30000; x++)
+                {
+                    Console.WriteLine(x);
+                }
+                Output = i.TwoTimes().ToString();
+            });
+        }
+    }
+
+    public static class TaskExtender
+    {
+        public async static void AwaitVoid(this Task task, Action completed, Action<Exception> errored)
+        {
+            try
+            {
+                await task;
+
+                completed?.Invoke();
+            }
+            catch (Exception ex)
+            {
+                errored?.Invoke(ex);
+            }
+        }
+
+        public static int TwoTimes(this int i) => i * 2;
+    }
+
+    public class PropChange : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropChange([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TaskExtension
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDoTask_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button b)
+                if (b.CommandParameter is MainViewModel m)
+                    m.StartTask();
+        }
+    }
+}
